b3429049a1c32bef6fe5e132693e8a075edb5259
Allen Li <allen@allen.li>
2014-11-14 17:19:06 -0800

    sketch implementation of manifold

---
+++ Applications/manifold/manifold.py
@@ -0,0 +1,81 @@
+"""
+manifold.py
+
+A library for compacting actuator instructions.
+
+Example usage:
+
+a = Actuator.linear() # default linear actuator
+values = [0, 0, 1, 2, 3, 4, 5, 4, 3, 3, 3]
+command = a.get_commands(values)
+>>> command
+[(0, 0.5), (1, 1), (7, 0), (9, 0.5)]
+
+a = Actuator.linear(10) # actuator that can change at 10 units per ms
+values = [0, 10, 15, 20, 10, 0]
+command = a.get_commands(values)
+>>> command
+[(0, 1), (1, 0.75), (3, 0)]
+"""
+
+from __future__ import division # use python3 style division
+
+class Actuator(object):
+    """Actuator is a representation of an actuator's physical characteristics
+    """
+    def __init__(self, dh, dl):
+        self.delta_high = tuple(dh)
+        self.delta_low = tuple(dl)
+
+    @staticmethod
+    def linear(speed = 1):
+        """ Returns a linear actuator."""
+        dh = (speed,) * 256
+        dl = (-speed,) * 256
+        return Actuator(dh, dl)
+
+    def calc_strength(self, current_output, desired_delta):
+        """ calculates the strength that should be output
+        """
+        current_output = int(current_output)
+        delta_high = self.delta_high[current_output]
+        delta_low = self.delta_low[current_output]
+        if desired_delta > delta_high:
+            print "desired delta too high for actuator!"
+            return 1.0
+        if desired_delta < delta_low:
+            print "desired delta too low for actuator!"
+            return 0.0
+        return (desired_delta - delta_low) / (delta_high - delta_low)
+
+
+    def get_commands(self, values):
+        cv = compact(values)
+        commands = []
+        current_strength = 0
+        current_output = cv[0][1]
+        for i in xrange(len(cv) - 1): # iterate through each difference
+            cmd_pair = (cv[i], cv[i + 1])
+            current_time = cmd_pair[0][0]
+            d_time = cmd_pair[1][0] - cmd_pair[0][0]
+            d_value = cmd_pair[1][1] - current_output
+            desired_delta = d_time / d_value
+            strength = self.calc_strength(current_output, desired_delta)
+            if abs(strength - current_strength) > 0.000001:
+                commands.append((current_time, strength))
+                current_strength = strength
+        return commands
+
+def compact(values):
+    """compact takes an array of integer values and expresses them as a series of
+    (time, value) instructions
+    e.g. [10, 10, 10, 10, 10, 50, 10] -> [(0, 10), (5, 50), (6, 10)]
+    """
+    compacted = []
+    # remove extraneous values
+    prev = None
+    for i, v in enumerate(values):
+        if v != prev:
+            prev = v
+            compacted.append((i, v))
+    return compacted
