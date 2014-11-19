8eae2683e28e34e8a32f8c4d6ad17fa60ed1e25a
Akhila Raju <akhilaraju@berkeley.edu>
2014-11-18 23:17:48 -0800

    run experiment

--- Applications/JND/jnd.py
+++ Applications/JND/jnd.py
@@ -1,4 +1,5 @@
-import os, sys, time, datetime, json
+import os, sys, time, datetime, json, random, operator
+from operator import itemgetter
 
 #Create specified actuator folder and specified participant folder
 def newActuator(actuatorName, participantNumber):
@@ -24,22 +25,110 @@ def runExperiment(actuatorName, participantNumber):
 	# data['yValues'] = 
 	#buttonpresses
 	json_data = json.dumps(data)
+	postJson(json_data)
 
 #post json to URL
-#def postJson(json):
-	#url = "http://localhost:8080" #talk to Jasper about where to post, send Json text
-	# r = requests.post(url, data=jsonText), headers=headers)
+def postJson(json):
+	url = "http://localhost:8080"
+	r = requests.post(url, data=json)
 
-# def actuatorVis():
-# 	visualization that plots data points of all participants for an actuator
+def actuate(intensity):
+	#actuate at given intensity
+	print("actuating")
 
-# def participantVis():
-# 	visualize for a particular participant for an actuator
+def run_experiment():
+	ranges = []
+	values = []
+	# set of query points [0, 100]
+	for i in range(0, 101):
+		values.append(i)
+	
+	query = random.choice(values)
 
 
-actuatorName = raw_input('Enter Actuator Name: ')
-participantNumber = raw_input('Participant Number: ')
-runExperiment(actuatorName, participantNumber)
+	while not len(values) == 0:
+		print "Pre: ", values
+		jnd_range = jnd(query)
+		
+		print query, ": [", jnd_range, "]"
+		# remove query and 50% range from index
+		values = remove_range(values, jnd_range)
+		ranges.append([query, jnd_range])
+		if len(values) != 0:
+			query = random.choice(values)
+	sorted_ranges = sorted(ranges, key=itemgetter(0))
+	print sorted_ranges
+
+
+def remove_range(values, jnd_range):
+	down, up = jnd_range
+	up = up + 1 # correcting limits
+	for j in range(down, up):
+		# index of the candidate value
+		
+		try:
+			values.remove(j)
+		except ValueError:
+			pass
+	print "Post: ", values
+	return values
+	
+
+def jnd(query):
+	up = query + 5
+	up = 100 if up > 100 else up
+	down = query - 5
+	down = 0 if down < 0 else down
+
+	# while (!noticed):
+	# 	up+=1
+	# 	actuate(up)
+	# noticed = false
+	# while (!noticed):
+	# 	down-=1
+	# 	actuate(down)
+	# wait for button press for up
+	# wait for button press for down
+	# return the full range
+	jnd_range = [down, up]
+	return jnd_range
+
+def fifty(up, down, comparison):
+	fifty_up = (comparison - up)/2
+	fifty_down = (comparison - down)/2
+	return fifty_up, fifty_down
+
+# def find_threshold(point, alpha):
+# 	noticed = false #jnd noticed
+# 	up = down = point
+# 	while (!noticed):
+# 		up+=1
+# 		actuate(up)
+# 	noticed = false
+# 	while (!noticed):
+# 		down-=1
+# 		actuate(down)
+# 	return range(down, up)
+
+	# up_k = k_value(up, point)
+	# down_k = k_value(down, point)
+	# return [up_k, down_k]
+
+def k_value(just_noticed, original):
+	if just_noticed < original:
+		return (just_noticed - original)/original
+	else:
+		return (original - just_noticed)/original
+
+
+# remove_range([0, 1, 2, 3, 4], [3, 4]);
+# [0, 1, 2]
+run_experiment();
+
+
+# actuatorName = raw_input('Enter Actuator Name: ')
+# participantNumber = raw_input('Participant Number: ')
+# runExperiment(actuatorName, participantNumber)
 
 # add command args
 # -ls outputs all of the actuator names that we have on file
