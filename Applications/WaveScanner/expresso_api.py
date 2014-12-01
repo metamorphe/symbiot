0058544e9cadecd4a00c2ee6f1970d7a359c64b3
Kenny Wang <kennyw12@berkeley.edu>
2014-11-30 22:36:54 -0800

    kmeans

--- Applications/WaveScanner/expresso_api.py
+++ Applications/WaveScanner/expresso_api.py
@@ -1,1 +1,48 @@
-../Scheduler/expresso_api.py
+
+import requests, md5, json, requests_cache
+base = "http://localhost:3000"
+password = "potatoes123"
+# base = "http://expresso.cearto.com"
+
+def get(url):
+	print base + url
+	resp = requests.get(base + url)
+	return resp.json()
+
+def post(url, payload):
+	print base + url 
+	m = md5.new()
+	m.update(password)
+	auth = 	m.hexdigest()
+	# payload["auth"] = auth
+	print payload
+
+	headers = {'content-type': 'application/json'}
+	resp = requests.post(base + url, data=json.dumps(payload), headers=headers)
+	print resp.text
+	# print resp.json()
+
+# get behavior from online repo
+def get_actuator(id):
+	url = "/api/actuators/" 
+	return get(url)
+
+def get_behavior(id):
+	url = "/api/behaviors/" + str(id)
+	return get(url)
+
+def get_commands(id):
+	url = "/api/behaviors/" + str(id) + "/sparse.json"
+	return get(url)["sparse_commands"]
+
+def send_behavior(name, wave):
+	url = '/api/behaviors/scanner'
+	data = {"behavior":{
+				'name': name,
+        		'states': wave
+				}
+			}
+	post(url, data)
+ 
+requests_cache.install_cache("api_cache")
+# requests_cache.disabled()
--- Applications/WaveScanner/test_scanner.py
+++ Applications/WaveScanner/test_scanner.py
@@ -3,43 +3,77 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 import expresso_api as exp
-
+import scanner
+import sliceImage
+import sys
 
 
 def func(x, b, c, d):
-	return 1 * np.cos(b * x - c) + d
+    return 1 * np.cos(b * x - c) + d
 
 """ VISUALIZATION FUNCTIONS """
 
-def error(wave):
-	popt, pcov = curve_fit(func, x, wave)
-	print "cos(bx -c) + d: ", popt, "\ncovariance", pcov
-	return popt, pcov
+def error(x, wave):
+    popt, pcov = curve_fit(func, x, wave)
+    print "cos(bx -c) + d: ", popt, "\ncovariance", pcov
+    return popt, pcov
 
 def plot(x, expected, observed):
-	plt.figure()
-	plt.plot(x, expected, 'k-', label="Expected", linewidth=5)
-	plt.plot(x, observed, 'b-', label="Observed")
-	
+    plt.figure()
+    plt.plot(x, expected, 'k-', label="Expected", linewidth=5)
+    plt.plot(x, observed, 'b-', label="Observed")
+    
 
-	popt, pcov = error(observed)
+    # popt, pcov = error(x, observed)
 
-	plt.plot(x, func(x, *popt), 'r-',label="Fitted Curve") #
-	plt.legend()
-	plt.show()
+    # plt.plot(x, func(x, *popt), 'r-',label="Fitted Curve") #
+    plt.legend()
+    plt.show()
 
 def log_wave(name, wave):
-	# name = 'pyramid'
-	# wave = [0, 1, 2, 3, 4, 3, 2, 1, 0]
-	exp.send_behavior(name, wave)
+    # name = 'pyramid'
+    # wave = [0, 1, 2, 3, 4, 3, 2, 1, 0]
+    exp.send_behavior(name, wave)
 
+def print_wave(x, expected):
+    baseLine = np.zeros(len(x))
+    baseLine.fill(-1)
+    midLine = np.zeros(len(x))
+    plt.figure()
+    plt.axis("off")
+    plt.plot(x, expected, 'k-', label="Expected", linewidth=3)
+    plt.plot(x, baseLine, 'k-', label="baseLine", linewidth=3)
+    plt.plot(x, midLine, 'k-', label="midLine", linewidth=3)
+    # plt.legend()
+    plt.show()
 
 
 """ EXAMPLE USAGE """
-x = np.linspace(0, 6 * np.pi,50)
-y = func(x, 1, 0, 0)
-y = y / np.max(y)
-yn = y + 0.2 * np.random.normal(size=len(x))
+# x = np.linspace(0, 6 * np.pi,50)
+# y = func(x, 1, 0, 0)
+# y = y / np.max(y)
+# yn = y + 0.2 * np.random.normal(size=len(x))
 
 # PLOT WAVES, CALCULATE ERROR
-plot(x, y, yn)
+# plot(x, y, yn)
+# print_wave(x, y)
+
+
+def main(argv):
+    if len(argv) != 1:
+        print 'Please Specify an image.'
+        exit(1)
+    videoName = argv[0].split(".")[0]+".avi"
+    pts = sliceImage.sliceImage(argv[0], videoName)
+    # scan = scanner.Scanner(videoName)
+    # pts = scan.get_wave()
+    x = np.linspace(0, 6 * np.pi, len(pts))
+    y = func(x, 1, 0, 1)
+    y = y / np.max(y)
+    print pts
+    print len(pts), len(x), len(y)
+    plot(x,y,pts)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
