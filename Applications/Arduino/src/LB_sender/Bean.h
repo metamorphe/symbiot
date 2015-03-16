3ed4da3e16773ae83a785285651fadabb1770a43
Jasper O'Leary <jhaazpr@gmail.com>
2015-03-16 14:08:59 -0700

    Add the rest of the API, all currently skeltons.

--- Applications/Arduino/src/LB_sender/Bean.h
+++ Applications/Arduino/src/LB_sender/Bean.h
@@ -9,14 +9,23 @@
 #define Bean_h
 
 #include "Arduino.h"
+#include "Behavior.h"
 
 class Bean {
 
-    public:
+  public:
+    /* Primary use functions */
+    void actuate(Behavior *);
+
+    /* Networking auxiliary functions, used by controller */
     void setRolePingOut();
     void setRolePongBack();
           
-    private:
-
+  private:
+    // Actuator _actuator; // To be implemented
+    void actuateBlinkM(Behavior *); // Use BlinkM only in the mean time
+    char *macAddress;
+    Bean *neighbor;
+    
 };
 #endif
