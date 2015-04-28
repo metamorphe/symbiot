c0ac4dec83125e60004bcc25a318482d8d06f379
Cesar Torres <cearto@berkeley.edu>
2015-04-28 23:55:46 +0000

    smart story registration basic version works

---
+++ Applications/RailsApp/app/controllers/smart_story_controller.rb
@@ -0,0 +1,15 @@
+class SmartStoryController < ApplicationController
+	def register
+		file = File.read('devices.json')
+		data_hash = JSON.parse(file)
+		if data_hash.has_key?(params[:uuid])
+			data_hash[params[:uuid]] = params[:modalities]
+		else
+			data_hash[params[:uuid]] = params[:modalities]
+			render :json => params[:uuid].to_json
+			File.open('devices.json', 'w') do |f|
+				f.write(JSON.pretty_generate(data_hash))
+			end
+		end
+	end
+end
